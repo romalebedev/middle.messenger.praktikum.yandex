0bc1c00db049cd64e8be67f9083b864023cb180d
rklebede <RLebedev@stream.ru>
2021-07-25 14:40:48 +0300

    add utils and render first test page

---
+++ src/utils/render-DOM.ts
@@ -0,0 +1,8 @@
+import Block from "./block";
+
+export function renderDom(query: string, block: Block) {
+    const root: HTMLElement | null = document.querySelector(query);
+
+    root && root.appendChild(block.getContent());
+    return root;
+  } 
